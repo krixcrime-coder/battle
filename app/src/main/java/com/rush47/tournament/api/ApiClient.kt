61e63b74351434475e2485fd987a1831ecb0229f
krixcrime-coder <krixcrime@gmail.com>
2026-07-25 22:19:24 +0530

    Upload app/src/main/java/com/rush47/tournament/api/ApiClient.kt

--- app/src/main/java/com/rush47/tournament/api/ApiClient.kt
+++ app/src/main/java/com/rush47/tournament/api/ApiClient.kt
@@ -17,6 +17,9 @@ object ApiClient {
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(logging)
+        .connectTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+        .readTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
+        .writeTimeout(30, java.util.concurrent.TimeUnit.SECONDS)
         .build()
 
     val instance: ApiService by lazy {
